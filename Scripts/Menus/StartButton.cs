6d9399ce8e387e687a083a760820f91a493b17e3
JCoupalK <108779415+JCoupalK@users.noreply.github.com>
2022-07-14 19:44:50 -0400

    Update StartButton.cs

--- Scripts/Menus/StartButton.cs
+++ Scripts/Menus/StartButton.cs
@@ -1,15 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
-
      public void StartGame ()
     {
         Debug.Log("Start");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-
-
 }
