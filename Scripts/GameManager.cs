fe44672a011fa867cc2bd8e5c04042a565a0fc8e
JCoupalK <108779415+JCoupalK@users.noreply.github.com>
2022-07-14 19:40:46 -0400

    Add files via upload

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics.Contracts;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool gameHasEnded = false;
+    public float restartDelay = 3f;
+
+    
+
+    private void Start()
+    {
+        GetComponent<AudioSource>().enabled = false;
+    }
+
+    
+
+
+    public void Restart()
+    {
+     
+        SceneManager.LoadScene("Level01");
+    }
+
+
+    public void EndGame()
+    {
+            if (gameHasEnded == false)
+            {
+                GetComponent<AudioSource>().enabled = true;
+
+                gameHasEnded = true;
+
+                Debug.Log("GAME OVER");
+                Invoke("Restart", restartDelay);
+
+
+            }
+
+        
+    }
+
+ 
+}
