9270d287f6f567eb43b89f08184205f9c17ddfb3
JCoupalK <108779415+JCoupalK@users.noreply.github.com>
2022-07-14 19:37:30 -0400

    Update PlayerCollision.cs

--- Scripts/Player/PlayerCollision.cs
+++ Scripts/Player/PlayerCollision.cs
@@ -1,33 +1,17 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerCollision : MonoBehaviour
 {
     public PlayerMovement movement;
-    
-
-    
-
     void OnCollisionEnter (UnityEngine.Collision collisionInfo)
     {
-         
-
+        
         if (collisionInfo.gameObject.tag == "Obstacle")
-        {
-            
+        {           
             GetComponent<PlayerMovement>().enabled = false;
-
             FindObjectOfType<GameManager>().EndGame();
             
         }
-
-        
-
-
-
-
     }
-
-
-
 }
